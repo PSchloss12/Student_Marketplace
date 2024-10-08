8f307f572f3c314278e0576cd0ba959f6c986ee1
Patrick Schlosser <pschloss@nd.edu>
2024-10-08 16:41:10 -0400

    refactor Header page and add parse for backend

--- student-marketplace/src/App.js
+++ student-marketplace/src/App.js
@@ -1,5 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './Components/Main/Main.js'
+import Parse from "parse";
+import * as ENV from "./environment.js"
+
+Parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY);
+Parse.serverURL(ENV.SERVER_URL);
 
 function App() {
   return (
---
+++ student-marketplace/src/Components/Header/Header.js
@@ -0,0 +1,43 @@
+// This component will render the navigation bar or header for each page
+
+const Header = ({ onNavigate }) => {
+  return(
+    <div class="topappbar">
+      <div class="title">ND Marketplace</div>
+      <div class="navigation">
+        <button
+          class="button-52"
+          role="button"
+          onClick={() => onNavigate("mainPage")}
+        >
+          Homepage
+        </button>
+        <button
+          class="button-52"
+          role="button"
+          onClick={() => onNavigate("productPage")}
+        >
+          View Products
+        </button>
+        <button
+          class="button-52"
+          role="button"
+          onClick={() => onNavigate("sellerForm")}
+        >
+          List Item
+        </button>
+        <button
+          class="button-52"
+          role="button"
+          onClick={() => {
+            onNavigate("login");
+          }}
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
